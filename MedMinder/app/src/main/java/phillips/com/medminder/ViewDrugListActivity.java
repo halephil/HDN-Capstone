2bd5e08bef694f83d645d5f2993537f693d02f01
halephil <halen.s.phillips@gmail.com>
2017-10-10 10:52:51 -0500

    MedMinderTemplet
    This is the templet project for the Medminder application

---
+++ MedMinder/app/src/main/java/phillips/com/medminder/ViewDrugListActivity.java
@@ -0,0 +1,13 @@
+package phillips.com.medminder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewDrugListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_drug_list);
+    }
+}
