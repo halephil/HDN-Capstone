d660d5d2c2edfcabcccb3fa8be0d4e4368cd56d4
halephil <halen.s.phillips@gmail.com>
2017-10-24 10:45:06 -0500

    DrugListView
    -includes code for generate list items and populating list item fields

---
+++ MedMinder/app/src/main/java/phillips/com/medminder/Drug.java
@@ -0,0 +1,52 @@
+package phillips.com.medminder;
+
+/**
+ * Created by halenphillips on 10/10/17.
+ */
+
+public class Drug {
+
+    private String name;
+    private String description;
+    private int pillCount;
+
+
+    //Constructor
+    public Drug(String name, String description, int pillCount){
+        this.name = name;
+        this.description = description;
+        this.pillCount = pillCount;
+    }
+
+    //
+    public int decrementPillCount(){
+        if(pillCount <= 0)
+            return 0;
+
+        pillCount = pillCount - 1;
+        return pillCount;
+    }
+
+
+    //GETTERS SETTERS//
+
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public int getPillCount(){ return this.pillCount;}
+
+}
--- MedMinder/app/src/main/java/phillips/com/medminder/ViewDrugListActivity.java
+++ MedMinder/app/src/main/java/phillips/com/medminder/ViewDrugListActivity.java
@@ -2,12 +2,69 @@ package phillips.com.medminder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ViewDrugListActivity extends AppCompatActivity {
+
+    private List<Drug> DrugList = new ArrayList<Drug>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_drug_list);
+        populateDrugList();
+        populateListView();
+
+    }
+
+
+    private void populateDrugList(){
+        DrugList.add(new Drug("Drug1","This is Drug 1.", 10));
+        DrugList.add(new Drug("Drug2","This is Drug 1.", 10));
+        DrugList.add(new Drug("Drug3","This is Drug 1.", 10));
+        DrugList.add(new Drug("Drug4","This is Drug 1.", 10));
+        DrugList.add(new Drug("Drug5","This is Drug 1.", 10));
+        DrugList.add(new Drug("Drug6","This is Drug 1.", 10));
+        DrugList.add(new Drug("Drug7","This is Drug 1.", 10));
+        DrugList.add(new Drug("Drug8","This is Drug 1.", 10));
+        DrugList.add(new Drug("Drug9","This is Drug 1.", 10));
+        DrugList.add(new Drug("Drug10","This is Drug 1.", 10));
+    }
+
+    private void populateListView() {
+        ArrayAdapter<Drug> adapter = new MyAdapter();
+        ListView list = (ListView)findViewById(R.id.drugListView);
+        list.setAdapter(adapter);
+    }
+
+    private class MyAdapter extends ArrayAdapter<Drug> {
+        public MyAdapter() {
+            super(ViewDrugListActivity.this, R.layout.item_view, DrugList);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View itemView = convertView;
+            if (itemView == null) {
+                itemView = getLayoutInflater().inflate(R.layout.item_view, parent, false);
+            }
+
+            Drug currentDrug = DrugList.get(position);
+
+            TextView textDrugName = itemView.findViewById((R.id.item_drugName));
+            textDrugName.setText(currentDrug.getName());
+
+            TextView textPillCount = itemView.findViewById((R.id.item_pillCount));
+            textPillCount.setText("Pill Count: 123");
+
+            return itemView;
+
+        }
     }
 }
