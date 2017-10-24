49c062c72dc17bab0ac72b4713b7e30f39bf7e7d
DAVID WALKER <drw0469@yahoo.com>
2017-10-24 12:09:57 -0500

    Created AlarmActivity
    Created AlarmActivity and registed it in the manifest file.

---
+++ AlarmDemo/app/src/main/java/com/walker/alarmdemo/MainActivity.java
@@ -0,0 +1,96 @@
+package com.walker.alarmdemo;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    TimePicker mTimePicker;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTimePicker = (TimePicker) findViewById(R.id.timePicker);
+
+        findViewById(R.id.buttonSetAlarm).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Calendar calendar = Calendar.getInstance();
+
+                if(Build.VERSION.SDK_INT >= 23) {
+                    calendar.set(
+                            calendar.get(Calendar.YEAR),
+                            calendar.get(Calendar.MONTH),
+                            calendar.get(Calendar.DAY_OF_MONTH),
+                            mTimePicker.getHour(),
+                            mTimePicker.getMinute(),
+                            0);
+                }else{
+                    calendar.set(
+                            calendar.get(Calendar.YEAR),
+                            calendar.get(Calendar.MONTH),
+                            calendar.get(Calendar.DAY_OF_MONTH),
+                            mTimePicker.getCurrentHour(),
+                            mTimePicker.getCurrentMinute(),
+                            0);
+                }
+
+                setAlarm(calendar.getTimeInMillis());
+
+            } // close onClick()
+        }); // close setOnClickListener
+    } // close onCreate()
+
+    private void setAlarm(long timeInMillis){
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+        Intent intent = new Intent(this, MyReceiver.class);
+
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0 , intent, 0);
+
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, timeInMillis, AlarmManager.INTERVAL_DAY, pendingIntent);
+
+        Toast.makeText(this, "Alarm is set", Toast.LENGTH_SHORT).show();
+    }
+
+} // close MainActivity
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ AlarmDemo/app/src/main/java/com/walker/alarmdemo/MyReceiver.java
@@ -0,0 +1,20 @@
+package com.walker.alarmdemo;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.provider.Settings;
+
+/**
+ * Created by Dave on 10/23/2017.
+ */
+
+public class MyReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        MediaPlayer mediaPlayer = MediaPlayer.create(context, Settings.System.DEFAULT_RINGTONE_URI);
+        mediaPlayer.start();
+    }
+}
